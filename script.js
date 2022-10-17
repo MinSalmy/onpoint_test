c1e02f236418a7da40c9bd2411759b34bab4ea7c
MinSalmy <nastarjob@mail.ru>
2022-10-18 00:17:32 +0700

    Конечная версия добавлены скрипты на все элементы добавлены стили на страницу 3 и переделаны стили на странице 2 удалена самая первая весрсия стилей style_p добавлены иконки для модального окна

--- script.js
+++ script.js
@@ -1,81 +1,139 @@
-const buttonHome = document.querySelector(".home");
 const buttonContinue = document.querySelector(".continue");
 const buttonDetails = document.querySelector(".details");
+const main = document.querySelector("main");
+const header = document.querySelector("header"); 
 
 buttonContinue.addEventListener('click', () => {
-    document.querySelector('.page-2').scrollIntoView({behavior: "smooth"});
+  document.querySelector('.page-2').scrollIntoView({ behavior: "smooth" });
 });
+header.addEventListener('click', () => {
+  document.querySelector('.page-1').scrollIntoView({ behavior: "smooth" });
+})
+/*
+  появление сперматозоидов версия 2
+*/
+/*
+main.addEventListener('scroll', (event) => {let index = 0;
+    if (event.target.scrollLeft % event.target.offsetWidth === 0) {
+    
+  spermImagesPage2.forEach(image => {
+    image.classList.toggle(`sperm-animation-${index % 2}`);
+    index++;
+  })
+  }
+});/*
+main.addEventListener('scroll', () => {
+  
+  console.log(window.scrollTo)
+}); */
 
+//скроллер
+const textPage2 = document.querySelector('.page-2 .text');
+const scrollbar = document.querySelector('.scrollbar');
+scrollbar.value = 0;
+scrollbar.min = 0;
+scrollbar.max = textPage2.scrollHeight - textPage2.clientHeight;
+scrollbar.addEventListener('input', () => {
+  textPage2.scrollTop = scrollbar.value;
+});
+textPage2.addEventListener('scroll', () => {
+  scrollbar.value = textPage2.scrollTop;
+})
 
 
 /*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  bubble random animation
 */
-//сделать либо слайдер для 3 слайда, либо попробовать на всю страницу, для 3 слайда добавить доты со стрелочками снизу
-class Slide {    
-    constructor (slides) {
+const bubbleImages = document.querySelectorAll('img[class^="bubble-"]');
+bubbleImages.forEach(bubble => {
+  const randomFinal = ["-10", "-20", "-30", "10", "20", "30"];
+  const randomNumberX = Math.floor(Math.random() * randomFinal.length);
+  const randomPercentX = randomFinal[randomNumberX];
+  const randomNumberY = Math.floor(Math.random() * randomFinal.length);
+  const randomPercentY = randomFinal[randomNumberY];
+  bubble.animate([
+    // keyframes
+    { transform: `none` },
+    { transform: `translateX(${randomPercentX}%) translateY(${randomPercentY}%) scale(1.1)` },
+  ], {
+    // timing options
+    duration: Math.floor(Math.random() * 3 + 2) * 1000,
+    iterations: 2000000, // "Infinity"
+    direction: "alternate",
+  })
+})
+
+//обработка диалогового окна
+const detailsButton = document.querySelector('.details.action');
+const dialogAdvantages = document.querySelector('.dialog-advantages');
+detailsButton.addEventListener('touchend', () => {
+  dialogAdvantages.show();  
+});
+dialogAdvantages.addEventListener('touchend', (event) => {if (event.target === dialogAdvantages) dialogAdvantages.close();});
+document.querySelector('.close-button').addEventListener('touchend', () => {
+  dialogAdvantages.close();
+})
+
+// каруселька
+class Carousel {    
+    constructor (slides, dots) {
         this.slides = slides;
+        this.dots = dots;
         this.currentSlide = 0;
         this.amount = slides.length;
     }
-    nextSlide() {
+    #toggleClass(elementPosition, classname) {
+      this.slides[elementPosition].classList.toggle(classname);
+      this.dots[elementPosition].classList.toggle(classname);
+
+      /*this.slides[this.currentSlide].classList.toggle('active');
+      this.dots[this.currentSlide].classList.toggle('active');*/
+    }
+    nextSlide() {console.log('work')
         if (this.currentSlide === this.amount - 1) return;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active');
+        /*this.#toggleClass();*/
         this.currentSlide++;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active'); 
+                
     }
     prevSlide() {
         if (this.currentSlide === 0) return;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active');
         this.currentSlide--;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active'); console.log('work')
     }
     setSlide(slideIndex) {
         if (slideIndex >= this.amount || slideIndex < 0) return;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active');
         this.currentSlide = slideIndex;
-        this.slides[this.currentSlide].classList.toggle('active');
+        this.#toggleClass(this.currentSlide, 'active');
     }
 }
 
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const carousel = new Carousel(slides, dots);
+for (let index = 0; index < dots.length; index++) dots[index].addEventListener('click', carousel.setSlide[index]);
+document.querySelector('.prev').addEventListener('click', () => {carousel.prevSlide(); console.log('clicked')});
+document.querySelector('.next').addEventListener('click', () => {carousel.nextSlide(); console.log('clicked')});
+
+/*
+  появление сперматозоидов версия 1
+*/
 const spermImagesPage2 = document.querySelectorAll('.page-2 img[class^="sperm-"]');
-console.log(spermImagesPage2);
-// Создать наблюдателя
 const observer = new IntersectionObserver(entries => {
     let index = 0;
     // перебор записей
     if (entries[0].isIntersecting) {
       spermImagesPage2.forEach(image => {
-        image.classList.toggle(`.sperm-animation-${index % 2}`);
+        image.classList.toggle(`sperm-animation-${index % 2}`);
         index++;
       })
     }
 });
-
-// Сообщить наблюдателю, какие элементы следует отслеживать
 observer.observe(document.querySelector('.sperm-1'));
+
+
+
+
